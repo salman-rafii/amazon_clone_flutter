1c893a3657e8b2b2bd11870abde37d2c5423d3dd
salman-rafii <salmanrafi125@gmail.com>
2023-01-07 01:02:13 +0500

    working on order details

--- lib/features/account/widgets/orders.dart
+++ lib/features/account/widgets/orders.dart
@@ -2,6 +2,7 @@ import 'package:amazon_clone_flutter/constants/global_variables.dart';
 import 'package:amazon_clone_flutter/constants/loader.dart';
 import 'package:amazon_clone_flutter/features/account/services/account_services.dart';
 import 'package:amazon_clone_flutter/features/account/widgets/single_product.dart';
+import 'package:amazon_clone_flutter/features/order_details/screens/order_details_screen.dart';
 import 'package:amazon_clone_flutter/models/order.dart';
 import 'package:flutter/material.dart';
 
@@ -75,11 +76,11 @@ class _OrdersState extends State<Orders> {
                   itemBuilder: (context, index) {
                     return GestureDetector(
                       onTap: () {
-                        // Navigator.pushNamed(
-                        //   context,
-                        //   OrderDetailsScreen.routeName,
-                        //   arguments: orders![index],
-                        // );
+                        Navigator.pushNamed(
+                          context,
+                          OrderDetailsScreen.routeName,
+                          arguments: orders![index],
+                        );
                       },
                       child: SingleProduct(
                         image: orders![index].products[0].images[0],
--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -4,8 +4,10 @@ import 'package:amazon_clone_flutter/features/admin/screens/add_product_screen.d
 import 'package:amazon_clone_flutter/features/auth/screens/auth_screen.dart';
 import 'package:amazon_clone_flutter/features/home/screens/category_deals_screen.dart';
 import 'package:amazon_clone_flutter/features/home/screens/home_screen.dart';
+import 'package:amazon_clone_flutter/features/order_details/screens/order_details_screen.dart';
 import 'package:amazon_clone_flutter/features/product_details/screens/product_detail_screen.dart';
 import 'package:amazon_clone_flutter/features/search/screens/search_screen.dart';
+import 'package:amazon_clone_flutter/models/order.dart';
 import 'package:amazon_clone_flutter/models/product.dart';
 import 'package:amazon_clone_flutter/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
@@ -54,6 +56,13 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(
         builder: (_) => AddressScreen(
           totalAmount: totalAmount,
+        ),
+      );
+    case OrderDetailsScreen.routeName:
+      var order = settings.arguments as Order;
+      return MaterialPageRoute(
+        builder: (_) => OrderDetailsScreen(
+          order: order,
         ),
       );
     default:
